24b95ea8798b854aa025467734658537bc82f02f
Saatvik <133893171+saatvik1512@users.noreply.github.com>
2023-08-17 09:22:29 +0530

    made a range slider

--- etch.js
+++ etch.js
@@ -1,6 +1,10 @@
 //ask user to enter the size of grid
 const userGrid = prompt("Enter the square per grid", 2);
-
+const range = document.getElementById('range-slider');
+const rangeShower = document.querySelector('.rangeSliderValue');
+range.addEventListener('input', () => {
+    rangeShower.textContent = range.value;
+})
 // when user enters a input make that much size of grid
 const divContainer = document.querySelector('.drawing-container');
 for (let index = 0; index < userGrid * userGrid; index++){
