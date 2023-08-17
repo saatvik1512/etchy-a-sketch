3c809df8e7cf37fe5abd46a4d262141fe5f21c52
Saatvik <133893171+saatvik1512@users.noreply.github.com>
2023-08-17 11:17:12 +0530

    slider works

--- etch.js
+++ etch.js
@@ -1,18 +1,35 @@
-//ask user to enter the size of grid
-const userGrid = prompt("Enter the square per grid", 2);
+//selecting input range
 const range = document.getElementById('range-slider');
+
+//selecting paragraph
 const rangeShower = document.querySelector('.rangeSliderValue');
+
+//selecting main container
+const divContainer = document.querySelector('.drawing-container');
+
+//default 2 * 2 grid at start
+createDivs(range.value);
+
+//when slided => shows the slider's value
 range.addEventListener('input', () => {
     rangeShower.textContent = range.value;
+    const amount = range.value;
+    //when event is triggered => removes the old divs and replaces it with new div
+    divContainer.innerHTML = "";
+    createDivs(amount);
 })
-// when user enters a input make that much size of grid
-const divContainer = document.querySelector('.drawing-container');
-for (let index = 0; index < userGrid * userGrid; index++){
-    //create a div element and select main container
-    const div = document.createElement('div');
-    div.setAttribute('class', 'grid-blocks');
-    div.style.border = "1px solid grey"
-    div.style.width = "calc(" + (divContainer.clientWidth/userGrid) + "px" + " - " + 2 + "px" + ")";
-    div.style.height = "calc(" + (divContainer.clientHeight/userGrid) + "px" + " - " + 2 + "px" + ")";
-    divContainer.appendChild(div)
+
+function createDivs(userGrid){
+    for (let index = 0; index < userGrid**2; index++){
+        //create a div element and select main container
+        const div = document.createElement('div');
+        div.setAttribute('class', 'grid-blocks');
+        div.style.border = "1px solid grey"
+        div.style.width = "calc(" + (divContainer.clientWidth/userGrid) + "px" + " - " + 2 + "px" + ")";
+        div.style.height = "calc(" + (divContainer.clientHeight/userGrid) + "px" + " - " + 2 + "px" + ")";
+        divContainer.appendChild(div)
+    }
 }
+
+//create a func
+    // deletes all previous divs inside divContainer;
